fd6f548c93652db462ff8ffea47f5f1797e53a49
Glen Davies <glen.davies@a8c.com>
2020-09-11 12:30:23 +1200

    switch to filtering patterns in component rather than hook

--- packages/block-editor/src/components/inserter/quick-inserter.js
+++ packages/block-editor/src/components/inserter/quick-inserter.js
@@ -134,7 +134,7 @@ function QuickInserter( {
 		blockTypeCollections,
 		onSelectBlockType,
 	] = useBlockTypesState( destinationRootClientId, onInsertBlocks );
-	const [ , patterns, , onSelectBlockPattern ] = usePatternsState(
+	const [ patterns, , onSelectBlockPattern ] = usePatternsState(
 		onInsertBlocks,
 		'all'
 	);
